d3b66cc8e08e4983eaf9de9933b470f406b85158
Juan-402 <juanito40222@gmail.com>
2024-12-27 15:07:13 -0500

    Cambios Herd Definitivo

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use Tighten\Ziggy\Ziggy as ZiggyZiggy;
 use Tightenco\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware
@@ -35,7 +36,7 @@ class HandleInertiaRequests extends Middleware
                 'user' => $request->user(),
             ],
             'ziggy' => function () use ($request) {
-                return array_merge((new Ziggy())->toArray(), [
+                return array_merge((new ZiggyZiggy())->toArray(), [
                     'location' => $request->url(),
                 ]);
             },
